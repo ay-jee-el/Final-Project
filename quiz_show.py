0c40cd47c9d53588c656259f22477d1e71fdfa8b
Alexa Lee <ayjeeel@gmail.com>
2024-11-18 00:13:54 -0600

    more notes of pseudocode (mostly how to add score to correct answer, what I need to know about tkinter), made some adjustments to parameters in init

--- quiz_show.py
+++ quiz_show.py
@@ -2,30 +2,34 @@ import tkinter as tk
 from quiz_questions import quiz_qna
 
 class Quiz:
-    def __init__(self):
+    def __init__(self, question = 0, score = 0):
         # boots up quiz? 
 
-        self.question = 0
-        self.score = 0
+        self.question = question
+        self.score = score
         self.questions = quiz_qna
 
     # questions, answer, choiches, LAYOUT for all this, calucations for correct nd wrong, output
     # ^^ will include aspects of the quiz. likle ywilll need various defs
         def layout(self):
-            #layout and imports for tkinter layout
+            #layout and imports for tkinter layout (look into this)
+            self.lable =
 
         def check_answer(self):
             # if check if the "choice" olines up with what was clicked
+            # if answer == "answer", then score += 1 (?)
 
         def next(self):
             #moves on the next question
-            
+
         def calculate(self):
             # math for how many correct
 
 
 
-def main():
-    #main file!! will import clases into here and display/ run?
-
-    print(Quiz)
+if __name__ == "__main__":
+    # Seems like tkinter operates differently than traditional pygames main? Using notes and info about tkinter for this part
+    # Dont mess w for now
+    root = tk.Tk()
+    quiz = Quiz(root)
+    root.mainloop
