12e090353618f53ad365f065f4c79a2d13575ec2
Alexa Lee <ayjeeel@gmail.com>
2024-11-17 23:41:26 -0600

    made .py file for quiz questuons, choices, and answers as it seems tkinter does not use .csv?

---
+++ quizshow.py
@@ -0,0 +1,12 @@
+# Creating a quiz show!!!! But how?
+# Seeing people use tkinter for interfaces like this? Or stick w pygames...depends
+
+# import .csv file (mostly to make cleanup easier?)
+# Create classes
+
+
+def main():
+    # Where the main code will run! like rain.py, well probably bring in classes?
+
+if __name__ == "__main__":
+    main()
