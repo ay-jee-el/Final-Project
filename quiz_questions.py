bcc842ad994428326154cb370bf9a637a26d86ab
Alexa Lee <ayjeeel@gmail.com>
2024-11-17 23:42:18 -0600

    addit: added psedudocde (may need to expand on HOW to execute more spec.)

---
+++ quiz_questions.py
@@ -0,0 +1,71 @@
+quiz_qna = [ 
+    {
+        # 1
+        "question": "What letter doesn't appear in a U.S State's name?", 
+        "options": ["Z", "X", "Q", "Y"],
+        "answer": "Q"
+    },
+
+    {
+        # 2
+        "question": "Which of these animals did Roosevelt NOT own?", 
+        "options": ["Z", "X", "Q", "Y"],
+        "answer": "Sheep"
+    },
+
+    {
+        # 3
+        "question": "Who was the 14th President of The United States?", 
+        "options": ["Z", "X", "Q", "Y"],
+        "answer": "Franklin Pierce"
+    },
+
+    {
+        # 4
+        "question": "WWhat is the national animal of Rhode Island?", 
+        "options": ["Z", "X", "Q", "Y"],
+        "answer": "The Haror Seal"
+    },
+
+    {
+        # 5
+        "question": "Which state was German chocolate cake made", 
+        "options": ["Z", "X", "Q", "Y"],
+        "answer": "Texas"
+    },
+
+    {
+        # 6
+        "question": "What is the official language of the United States", 
+        "options": ["Z", "X", "Q", "Y"],
+        "answer": "None"
+    },
+
+    {
+        # 7
+        "question": "Idaho is also known as...?", 
+        "options": ["Z", "X", "Q", "Y"],
+        "answer": "The Gem State"
+    },
+
+    {
+        # 8
+        "question": "What is the most used color in the state flags", 
+        "options": ["Z", "X", "Q", "Y"],
+        "answer": "Blue"
+    },
+
+    {
+        # 9
+        "question": "What is the ZIP Code of the Empire State Building?", 
+        "options": ["Z", "X", "Q", "Y"],
+        "answer": "10188"
+    },
+
+    {
+        # 10
+        "question": "What is the most popular surname in California?", 
+        "options": ["Z", "X", "Q", "Y"],
+        "answer": "Garcia"
+    },
+]
---
+++ quiz_show.py
@@ -0,0 +1,5 @@
+# Creating a quiz show!!!! But how?
+# Seeing people use tkinter for interfaces like this?
+
+# import .csv file (mostly to make cleanup easier?) (nevermind. need to use a .py file)
+# Create classes. do i just need one, or multiple? if one, i need to run in loop
