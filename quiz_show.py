53ee0dd62b36c1a1519f4dd17de93e320f25a0f0
Alexa Lee <ayjeeel@gmail.com>
2024-11-17 23:48:26 -0600

    imported file with questions into main file, debugging to see it'll run smoothly

--- quiz_show.py
+++ quiz_show.py
@@ -3,3 +3,7 @@
 
 # import .csv file (mostly to make cleanup easier?) (nevermind. need to use a .py file)
 # Create classes. do i just need one, or multiple? if one, i need to run in loop
+
+from quiz_questions import quiz_qna
+
+print(quiz_qna)
