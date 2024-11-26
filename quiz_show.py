ab7468cb6be108bbb326fbc21471ae349a8709a4
Alexa Lee <ayjeeel@gmail.com>
2024-11-25 23:58:27 -0600

    readjusted some naming conventions for functions, started working on layout custom function

--- quiz_show.py
+++ quiz_show.py
@@ -2,18 +2,26 @@ import tkinter as tk
 from quiz_questions import quiz_qna
 
 class Quiz:
-    def __init__(self, question = 0, score = 0):
+    def __init__(self, master, score = 0):
         # boots up quiz? 
-
-        self.question = question
+        self.master = master
+        
+        self.question_number = 0
         self.score = score
         self.questions = quiz_qna
 
+        self.layout()
+
     # questions, answer, choiches, LAYOUT for all this, calucations for correct nd wrong, output
     # ^^ will include aspects of the quiz. likle ywilll need various defs
-        def layout(self):
+    def layout(self):
             #layout and imports for tkinter layout (look into this)
-            self.lable =
+        self.lable = tk.Label(self.master, text=self.questions)
+        self.lable.pack()
+
+        self.var = tk.StringVar()
+
+        for option in self.questions:
 
         def check_answer(self):
             # if check if the "choice" olines up with what was clicked
