6d0d3a79960d0c9bb7bdc070f6bb3fb8f552baa3
Alexa Lee <ayjeeel@gmail.com>
2024-11-26 00:38:46 -0600

    fixed minor errors like paratheses in some lines missing, spelling mistakes, and fixing lable to label

--- quiz_show.py
+++ quiz_show.py
@@ -17,12 +17,12 @@ class Quiz:
     # ^^ will include aspects of the quiz. likle ywilll need various defs
     def layout(self):
             #layout and imports for tkinter layout (look into this)
-        self.lable = tk.Label(self.master, text=self.questions[self.qn]["question"])
-        self.lable.pack(pady=10)
+        self.label = tk.Label(self.master, text=self.questions[self.qn]["question"])
+        self.label.pack(pady=10)
 
         self.var = tk.StringVar()
 
-        for option in self.questions[self.qn]["question"]:
+        for option in self.questions[self.qn]["options"]:
             rb = tk.Radiobutton(self.master, text=option, variable=self.var, value=option)
             rb.pack()
 
@@ -35,20 +35,20 @@ class Quiz:
             if self.var.get() == self.questions[self.qn]["correct"]:
                 self.score += 1
 
-            if self.qn < len(self.qustions):
-                 self.next
+            if self.qn < len(self.questions):
+                 self.next()
             else:
-                 self.calculate
+                 self.calculate()
 
     def next(self):
             #moves on the next question
-            self.lable.config(text=self.questions[self.qn]["question"])
+            self.label.config(text=self.questions[self.qn]["question"])
             # unsure how this will work; look into it!!
             for wgt in self.master.winfo_children():
                  if isinstance(wgt, tk.Radiobutton):
                       wgt.destroy()
             
-            for option in self.questions[self.qn]["question"]:
+            for option in self.questions[self.qn]["options"]:
                  radiobutton = tk.Radiobutton(self.master, text=option, variable=self.var, value=option)
                  radiobutton.pack()
 
@@ -62,4 +62,4 @@ if __name__ == "__main__":
     # Dont mess w for now
     root = tk.Tk()
     quiz = Quiz(root)
-    root.mainloop
+    root.mainloop()
