c86fbf31891f18464c044cd46eea7a81b069fab1
Alexa Lee <ayjeeel@gmail.com>
2024-12-04 19:56:44 -0600

    Deleted pseudocode

--- quiz_show.py
+++ quiz_show.py
@@ -3,7 +3,6 @@ from tkinter import messagebox
 from quiz_questions import quiz_qna
 
 class Quiz:
-    #Doesnt appear? runs w no errors tho
     def __init__(self, master):
         # boots up quiz? 
         self.master = master
@@ -13,10 +12,7 @@ class Quiz:
 
         self.layout()
 
-    # questions, answer, choiches, LAYOUT for all this, calucations for correct nd wrong, output
-    # ^^ will include aspects of the quiz. likle ywilll need various defs
     def layout(self):
-            #layout and imports for tkinter layout (look into this)
         root.geometry("600x250")
         root.configure(bg="lightblue")
         self.label = tk.Label(self.master, text=self.questions[self.qn]["question"], fg="darkblue", bg="lightblue", font=("Helvetica", 14))
@@ -43,9 +39,7 @@ class Quiz:
                  self.calculate()
 
     def next(self):
-            #moves on the next question
             self.label.config(text=self.questions[self.qn]["question"])
-            # unsure how this will work; look into it!!
             for wgt in self.master.winfo_children():
                  if isinstance(wgt, tk.Radiobutton):
                       wgt.destroy()
@@ -55,19 +49,10 @@ class Quiz:
                  radiobutton.pack()
 
     def calculate(self):
-            # math for how many correct
         messagebox.showinfo("Complete!",f"Your score: {self.score}/{len(self.questions)}")
         self.master.destroy()
 
 if __name__ == "__main__":
-    # nsure why if is messing it up?
-    # Dont mess w for now
     root = tk.Tk()
     quiz = Quiz(root)
     root.mainloop()
-
-# Succesfully works! But now have to figure out:
-     # Why the first question is formatted oddly
-     # How to have the question box remain the same size
-     # Maybe some color and flair?
-     # spelling errors in questions and answers
